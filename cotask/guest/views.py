742552a3ad26e16c63cdcc723391f25b0051d8f6
she1kopr9d <she1kopr9d@icloud.com>
2025-06-08 13:37:59 +0300

    Add html templates - 0.0.3

--- cotask/guest/views.py
+++ cotask/guest/views.py
@@ -1,5 +1,8 @@
 import django.shortcuts
+import django.http
 
 
-def index(request):
+def index(request: django.http.HttpRequest):
+    if request.user.is_authenticated:
+        return django.shortcuts.redirect("feed")
     return django.shortcuts.render(request, "guest/main.html")
